89e6c84dbd8c63408dc5d21c11636e129d5235d6
Matt <matfsw@gmail.com>
2011-04-06 06:35:15 +0200

    Added method proposals from supertypes

--- src/org/concordion/ide/eclipse/assist/MethodProposalProvider.java
+++ src/org/concordion/ide/eclipse/assist/MethodProposalProvider.java
@@ -3,15 +3,20 @@ package org.concordion.ide.eclipse.assist;
 import static java.util.Collections.emptyList;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.concordion.ide.eclipse.JdtUtils;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ITypeHierarchy;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.Signature;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 
 public class MethodProposalProvider implements ProposalProvider {
@@ -30,7 +35,7 @@ public class MethodProposalProvider implements ProposalProvider {
 
 	@Override
 	public List<ICompletionProposal> provideProposal(AssistContext assistContext, String namespacePrefix, int offset) {
-		return createMethodProposals(offset, propsalPrefix );
+		return createMethodProposals(offset, propsalPrefix);
 	}
 	
 	private List<ICompletionProposal> createMethodProposals(int offset, String prefix) {
@@ -47,40 +52,86 @@ public class MethodProposalProvider implements ProposalProvider {
 	private static List<ICompletionProposal> createAccessibleMethodProposals(IType type, int offset, String prefix) {
 		List<ICompletionProposal> proposals = new ArrayList<ICompletionProposal>();
 		try {
-			for (IMethod method : type.getMethods()) {
-				int flags = method.getFlags();
-				if (Flags.isPublic(flags) || Flags.isProtected(flags) || Flags.isPackageDefault(flags)) {
-					proposals.add(createMethodProposal(method, offset, prefix));
-				}
-			}
+			Set<String> addedMethods = new HashSet<String>();
+			addMethodProposals(type, type.getPackageFragment().getElementName(), offset, prefix, proposals, addedMethods);
 			return proposals;
 		} catch (JavaModelException e) {
 			return emptyList();
 		}
 	}
 
-	private static ICompletionProposal createMethodProposal(IMethod method, int offset, String prefix) throws JavaModelException {
+	private static void addMethodProposals(IType type, String containingPkg, int offset, String prefix, List<ICompletionProposal> proposals, Set<String> addedMethods) throws JavaModelException {
+		for (IMethod method : type.getMethods()) {
+			if (!method.exists() || method.isConstructor()) {
+				continue;
+			}
+			
+			int flags = method.getFlags();
+			if (Flags.isPublic(flags) || Flags.isProtected(flags) || isPackageAccessible(type, containingPkg, flags)) {
+				createMethodProposal(method, containingPkg, offset, prefix, proposals, addedMethods);
+			}
+		}
+
+		// Recursively add accessible methods from supertype
+		ITypeHierarchy superTypeHierarchy = type.newSupertypeHierarchy(new NullProgressMonitor());
+		IType superType = superTypeHierarchy.getSuperclass(type);
+		if (superType != null && superType.exists() && !Object.class.getName().equals(superType.getFullyQualifiedName())) {
+			addMethodProposals(superType, containingPkg, offset, prefix, proposals, addedMethods);
+		}
+	}
+
+	private static boolean isPackageAccessible(IType type, String containingPkg, int flags) {
+		return containingPkg.equals(type.getPackageFragment().getElementName()) && Flags.isPackageDefault(flags);
+	}
+
+	private static void createMethodProposal(IMethod method, String containingPkg, int offset, String prefix, List<ICompletionProposal> proposals, Set<String> addedMethods) throws JavaModelException {
 		String methodName = method.getElementName();
 		String choice = methodName;
 		if (prefix != null) {
 			choice = prefix + choice;
 		}
 		
-		String params = parameters(method);
-		String proposal = choice + params;
-		return ProposalSupport.createProposal(
-				methodName, proposal, offset, choice.length() + 1, proposal.length(), ProposalSupport.NO_REPLACEMENT, ProposalIcon.METHOD);
+		StringBuilder params = new StringBuilder();
+		StringBuilder displayParams = new StringBuilder();
+		parameters(method, params, displayParams);
+		String proposal = choice + params.toString();
+		String displayProposal = choice + displayParams.toString();
+		
+		// Avoid adding same proposal twice for overridden methods
+		// Using displayProposal for discrimination as it contains type information (to catch overloaded methods)
+		if (addedMethods.contains(displayProposal)) {
+			return;
+		}
+		
+		addedMethods.add(proposal);
+		
+		proposals.add(ProposalSupport.createProposal(
+				displayProposal, proposal, offset, choice.length() + 1, proposal.length(), ProposalSupport.NO_REPLACEMENT, ProposalIcon.METHOD));
 	}
 
-	private static String parameters(IMethod method) throws JavaModelException {
-		StringBuilder str = new StringBuilder("(");
+	private static void parameters(IMethod method, StringBuilder proposalStr, StringBuilder displayStr) throws JavaModelException {
+		appendTwice(proposalStr, displayStr, "(");
+		String[] types = method.getParameterTypes();
+		int curType = 0;
 		for (String param : method.getParameterNames()) {
-			if (str.length() > 1) {
-				str.append(", ");
+			if (proposalStr.length() > 1) {
+				appendTwice(proposalStr, displayStr, ", ");
 			}
-			str.append('#').append(param);
+
+			String typeSignature = types[curType++];
+			String readableType = Signature.toString(typeSignature);
+			displayStr.append(readableType).append(' ');
+
+			proposalStr.append("#");
+			
+			appendTwice(proposalStr, displayStr, param);
 		}
-		return str.append(")").toString();
+		appendTwice(proposalStr, displayStr, ")");
+	}
+
+	private static void appendTwice(StringBuilder str, StringBuilder displayStr, String toAppend) {
+		str.append(toAppend);
+		displayStr.append(toAppend);
 	}
 
 }
